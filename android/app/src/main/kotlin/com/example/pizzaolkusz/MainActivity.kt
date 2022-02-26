c2d79b4986bd60f0437fe8157a5a3bc0e3b4a1dd
pruch10 <pruch10@gmail.com>
2022-02-26 21:04:06 +0100

    install firebase_auth

---
+++ android/app/src/main/kotlin/com/example/pizzaolkusz/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dominikprusek.pizzaolkusz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -11,13 +18,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        primarySwatch: Colors.blue,
-      ),
-      home:
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: RootPage());
   }
 }
 
+class RootPage extends StatelessWidget {
+  const RootPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          final user = snapshot.data;
+          if (user == null) {
+            return const Scaffold(
+              body: Center(
+                child: Text('Jesteś niezalogowany'),
+              ),
+            );
+          }
+          return Scaffold(
+            body: Center(
+              child: Text('Jesteś zalogowany jako${user.email}'),
+            ),
+          );
+        });
+  }
+}
