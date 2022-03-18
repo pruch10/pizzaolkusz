bd1cc467857edb6f85211fb5b03af34e6a47b37a
pruch10 <pruch10@gmail.com>
2022-03-18 13:18:31 +0100

    Move myApp to app.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:pizzaolkusz/main.dart';
+import 'package:pizzaolkusz/app/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
