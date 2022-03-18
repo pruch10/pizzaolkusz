f086e0741f06b412b43ad99f5e780d3213ccb453
pruch10 <pruch10@gmail.com>
2022-03-18 14:06:50 +0100

    cubit LoginPage

---
+++ lib/app/cubit/root_cubit.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+part 'root_state.dart';
+
+class RootCubit extends Cubit<RootState> {
+  RootCubit()
+      : super(RootState(
+          user: null,
+          errorMessage: '',
+          isLoading: false,
+        ));
+
+  StreamSubscription? _streamSubscription;
+
+  Future<void> start() async {
+    emit(
+      RootState(
+        isLoading: true,
+        errorMessage: '',
+        user: null,
+      ),
+    );
+    _streamSubscription =
+        FirebaseAuth.instance.authStateChanges().listen((user) {
+      emit(RootState(isLoading: false, errorMessage: '', user: user));
+    })
+          ..onError((error) {
+            emit(RootState(
+                isLoading: true, errorMessage: error.toString(), user: null));
+          });
+  }
+
+  @override
+  Future<void> close() {
+    _streamSubscription?.cancel();
+    return super.close();
+  }
+}
